526ce50ac249b6fb0f4487290ac3e42a44ce48fe
Wade He <wade.he@arbfintech.com>
2019-04-15 10:26:43 -0400

    PANDA-235 fetch portfolio information from runtime

---
+++ src/main/java/com/arbfintech/microservice/loan/client/RuntimeFeignClient.java
@@ -0,0 +1,17 @@
+package com.arbfintech.microservice.loan.client;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Wade He
+ */
+
+@FeignClient("cache-server")
+@RequestMapping("/runtime")
+public interface RuntimeFeignClient {
+	@GetMapping("/portfolio-parameters/{portfolioId}")
+	public String getPortfolioParameter(@PathVariable("portfolioId") Integer portfolioId);
+}
