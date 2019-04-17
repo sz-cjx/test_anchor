8349a5c1f35a3849ae6c5a5de788397c7746abc0
Richard <richard@arbfintech.com>
2019-04-17 09:35:26 -0400

    modify

--- src/main/java/com/arbfintech/microservice/loan/repository/LoanRepository.java
+++ src/main/java/com/arbfintech/microservice/loan/repository/LoanRepository.java
@@ -34,6 +34,8 @@ public interface LoanRepository extends JpaRepository<Loan, String>, JpaSpecific
     @Query(value="SELECT * FROM loan where withdrawn_code!='' AND update_time BETWEEN ?1 AND ?2",nativeQuery = true)
     public List<Loan> countWithDrawnloans(long startRecieveTime, long endReceiveTime);
 
-    @Query(value="SELECT * FROM loan where status=?1 and lead_id in (?2) ",nativeQuery = true)
-    public List<Loan> getLoanByStatusAndLeadId(Integer status, List<Integer> leadId);
+//    @Query(value = "SELECT * FROM loan where status=?1 and lead_id in ?2", nativeQuery = true)
+//    public List<Loan> getLoanByStatusAndLeadId(Integer status, List<Integer> leadId);
+
+    public List<Loan> findAllByStatusAndLeadIdIn(Integer status, List<Integer> leadId);
 }
