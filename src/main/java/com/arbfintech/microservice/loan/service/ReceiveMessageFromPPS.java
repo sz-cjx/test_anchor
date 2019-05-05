cb50828ea82927495f291b08c3fabc58e084aa54
Richard <richard@arbfintech.com>
2019-05-05 04:52:35 -0400

    FEATURE-1106/Receive the RabbitQ message  from PPS and change the loan status from approved to New

---
+++ src/main/java/com/arbfintech/microservice/loan/service/ReceiveMessageFromPPS.java
@@ -0,0 +1,62 @@
+package com.arbfintech.microservice.loan.service;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.arbfintech.component.core.constant.RabbitOperationConst;
+import com.arbfintech.component.core.constant.RabbitProducerConst;
+import com.arbfintech.component.core.enumerate.LoanStatusEnum;
+import com.arbfintech.component.core.message.RabbitMessage;
+import com.arbfintech.microservice.loan.controller.LeadReceiver;
+import com.arbfintech.microservice.loan.entity.Loan;
+import com.arbfintech.microservice.loan.repository.LoanRepository;
+import com.rabbitmq.client.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@RabbitListener(queues = RabbitProducerConst.MICROSERVICE_PAYMENT + "." + RabbitOperationConst.SEND_PAYMENT_TO_LOAN)
+public class ReceiveMessageFromPPS {
+
+    private Logger logger = LoggerFactory.getLogger(LeadReceiver.class);
+
+    @Autowired
+    private LoanRepository loanRepository;
+
+    @Autowired
+    private TimeLineApiService timeLineApiService;
+
+    @RabbitHandler
+    public void process(String content, Channel channel, Message message) {
+        try {
+            //告诉服务器已接受到消息，已被消费，可以在队列删除
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+            logger.info("receive payment data : {}", JSON.parseObject(content, RabbitMessage.class).getMessageData());
+
+            String contractNos = JSON.parseObject(content, RabbitMessage.class).getMessageData();
+            JSONArray contractArr = JSONArray.parseArray(contractNos);
+            if (channel != null && contractArr.size() != 0) {
+                for (Object contractNo : contractArr) {
+                    Loan loan=loanRepository.findByContractNo((String) contractNo);
+                    loan.setStatus(LoanStatusEnum.NEW.getValue());
+                    loanRepository.save(loan);
+                    JSONObject additionData = new JSONObject();
+                    additionData.put("contractNo", contractNo);
+                    timeLineApiService.addLoanStatusChangeTimeline(LoanStatusEnum.APPROVED.getValue(), LoanStatusEnum.NEW.getValue(), JSONObject.toJSONString(additionData));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.error("Handle lead data failed: ", e);
+        }
+    }
+}
+
+
