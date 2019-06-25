8960c453473d01e6e8127caa78a8e7dd4e93da3f
xiexia <crystal@arbfintech.com>
2019-06-25 05:40:15 -0400

    #790 Loan agreement doesn’t match with the Payment Plan

--- src/main/java/com/arbfintech/microservice/loan/LoanApplication.java
+++ src/main/java/com/arbfintech/microservice/loan/LoanApplication.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.TimeZone;
+
 @SpringBootApplication(scanBasePackages = {GlobalConst.SCAN_BASE_PACKAGE})
 @EnableEurekaClient
 @EnableJpaRepositories
@@ -14,6 +16,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class LoanApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
         SpringApplication.run(LoanApplication.class, args);
     }
 
