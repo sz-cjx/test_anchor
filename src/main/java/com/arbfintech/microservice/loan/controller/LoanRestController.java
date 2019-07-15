2c2ca1d0b7868ce90ef3d4dbef4125c1697e3acd
Richard <richard@arbfintech.com>
2019-07-15 13:04:02 -0400

    #1011

--- src/main/java/com/arbfintech/microservice/loan/controller/LoanRestController.java
+++ src/main/java/com/arbfintech/microservice/loan/controller/LoanRestController.java
@@ -247,7 +247,7 @@ public class LoanRestController {
                 String email = "";
                 if ("Approved Application".equals(status)) {
                     try {
-                        String title = "Sorry, we can not help you!";
+                        String title = "Your loan has been approved";
 
                         String tempaltes = maintenanceFeignClient.listEmailTemplateByPortfolioId(1);
                         JSONArray jsonArray = JSON.parseArray(tempaltes);
@@ -284,7 +284,7 @@ public class LoanRestController {
                         e.printStackTrace();
                     }
                 }else if("Positive Withdraw".equals(status) || "Negative Withdraw".equals(status)){
-                    String title = "Your loan has been refused!";
+                    String title = "Sorry, we can not help you";
 
                     String tempaltes = maintenanceFeignClient.listEmailTemplateByPortfolioId(1);
                     JSONArray jsonArray = JSON.parseArray(tempaltes);
