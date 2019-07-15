e2e7d06ac22f6de09db14d0dd5160204f3f9f8de
Richard <richard@arbfintech.com>
2019-07-15 14:27:22 -0400

    #1101/

--- src/main/java/com/arbfintech/microservice/loan/client/MaintenanceFeignClient.java
+++ src/main/java/com/arbfintech/microservice/loan/client/MaintenanceFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("microservice-maintenance")
+@FeignClient("maintenance-restapi")
 public interface MaintenanceFeignClient {
 
     @GetMapping("/holidays/{begin}/{end}")
