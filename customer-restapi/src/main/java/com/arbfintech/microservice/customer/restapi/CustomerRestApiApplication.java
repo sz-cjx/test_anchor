44f507ab68726509818400027492e63b015fd22a
wade <wade@arbfintech.com>
2019-11-26 16:23:43 -0500

    #2173 Update TimeZone

--- customer-restapi/src/main/java/com/arbfintech/microservice/customer/restapi/CustomerRestApiApplication.java
+++ customer-restapi/src/main/java/com/arbfintech/microservice/customer/restapi/CustomerRestApiApplication.java
@@ -7,12 +7,15 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication(scanBasePackages = GlobalConst.SCAN_BASE_PACKAGE)
 @EnableFeignClients(basePackages = GlobalConst.SCAN_BASE_PACKAGE)
 @EnableEurekaClient
 @EnableJpaAuditing
 public class CustomerRestApiApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
         SpringApplication.run(CustomerRestApiApplication.class,args);
     }
 }
