e3ca16995355c3768713d05bbb28d86050824502
zusi <1668180138@qq.com>
2020-06-11 20:47:19 +0800

    Update to new code style

--- customer-clientapi/src/main/java/com/arbfintech/microservice/customer/clientapi/controller/CustomerClientController.java
+++ customer-clientapi/src/main/java/com/arbfintech/microservice/customer/clientapi/controller/CustomerClientController.java
@@ -1,6 +1,8 @@
 package com.arbfintech.microservice.customer.clientapi.controller;
 
 import com.arbfintech.microservice.customer.clientapi.future.CustomerClientFuture;
+import com.arbfintech.microservice.customer.domain.entity.Customer;
+import com.arbfintech.microservice.customer.object.constant.ClientApiUrl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,31 +15,31 @@ public class CustomerClientController {
     @Autowired
     private CustomerClientFuture customerClientFuture;
 
-    @PostMapping("/replace-customer")
+    @PostMapping(ClientApiUrl.REPLACE_CUSTOMER)
     public CompletableFuture<Long> replaceCustomer(
             @RequestBody String dataStr
     ) {
         return customerClientFuture.replaceCustomer(dataStr);
     }
 
-    @GetMapping("/get-customer/{id}")
+    @GetMapping(ClientApiUrl.GET_CUSTOMER_BY_ID)
     public CompletableFuture<String> getCustomerById(
             @PathVariable("id") Long id
     ) {
         return customerClientFuture.getCustomerById(id);
     }
 
-    @GetMapping("/list-customer-ssn-email-num")
+    @GetMapping(ClientApiUrl.LIST_CUSTOMER_BY_SSN_OR_EMAIL_OR_NUM)
     public CompletableFuture<String> listCustomerBySsnOrEmailOrNo(
             @RequestParam("conditions") String conditionStr
     ) {
         return customerClientFuture.listCustomerBySsnOrEmailOrNo(conditionStr);
     }
 
-    @GetMapping("/get-customer")
-    public CompletableFuture<String> findCustomerByOptions(
+    @GetMapping(ClientApiUrl.GET_CUSTOMER_BY_CONDITIONS)
+    public CompletableFuture<String> getCustomerByCondition(
             @RequestParam("conditions") String conditionStr
     ) {
-        return customerClientFuture.findCustomerByCondition(conditionStr);
+        return customerClientFuture.getCustomerByCondition(conditionStr);
     }
 }
--- customer-clientapi/src/main/java/com/arbfintech/microservice/customer/clientapi/future/CustomerClientFuture.java
+++ customer-clientapi/src/main/java/com/arbfintech/microservice/customer/clientapi/future/CustomerClientFuture.java
@@ -1,7 +1,12 @@
 package com.arbfintech.microservice.customer.clientapi.future;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.arbfintech.framework.component.core.annotation.AsyncTimed;
-import com.arbfintech.microservice.customer.clientapi.service.CustomerClientService;
+import com.arbfintech.framework.component.core.constant.ConditionTypeConst;
+import com.arbfintech.framework.component.core.constant.JsonKeyConst;
+import com.arbfintech.framework.component.core.type.SqlOption;
+import com.arbfintech.microservice.customer.domain.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,31 +14,59 @@ import java.util.concurrent.CompletableFuture;
 
 /**
  * @author Baron
- *
  */
 @Component
 public class CustomerClientFuture {
 
     @Autowired
-    private CustomerClientService customerClientService;
+    private CustomerRepository customerRepository;
 
     @AsyncTimed
     public CompletableFuture<Long> replaceCustomer(String dataStr) {
-        return CompletableFuture.supplyAsync(() -> customerClientService.replaceCustomer(dataStr));
+        return CompletableFuture.supplyAsync(() -> replaceCustomers(dataStr));
     }
 
     @AsyncTimed
     public CompletableFuture<String> getCustomerById(Long id) {
-        return CompletableFuture.supplyAsync(() -> customerClientService.getCustomerById(id));
+        return CompletableFuture.supplyAsync(() -> findCustomerById(id));
     }
 
     @AsyncTimed
     public CompletableFuture<String> listCustomerBySsnOrEmailOrNo(String conditionStr) {
-        return CompletableFuture.supplyAsync(() -> customerClientService.listCustomerBySsnOrEmailOrNo(conditionStr));
+        return CompletableFuture.supplyAsync(() -> findCustomerListBySsnOrEmailOrNo(conditionStr));
     }
 
     @AsyncTimed
-    public CompletableFuture<String> findCustomerByCondition(String conditionStr) {
-        return CompletableFuture.supplyAsync(() -> customerClientService.findCustomerByCondition(conditionStr));
+    public CompletableFuture<String> getCustomerByCondition(String conditionStr) {
+        return CompletableFuture.supplyAsync(() -> findCustomerByCondition(conditionStr));
+    }
+
+    private Long replaceCustomers(String dataStr) {
+        return customerRepository.replaceCustomer(dataStr);
+    }
+
+    private String findCustomerById(Long id) {
+        return customerRepository.getCustomerById(id);
+    }
+
+    private String findCustomerListBySsnOrEmailOrNo(String conditionStr) {
+        JSONObject paramJson = JSON.parseObject(conditionStr);
+        String ssn = paramJson.getString(JsonKeyConst.SSN);
+        String email = paramJson.getString(JsonKeyConst.EMAIL);
+        String accountNo = paramJson.getString(JsonKeyConst.ACCOUNT_NO);
+        String routingNo = paramJson.getString(JsonKeyConst.ROUTING_NO);
+
+        SqlOption sqlOption = SqlOption.getInstance();
+        sqlOption.addWhereFormat(ConditionTypeConst.OR, "ssn = '%s'", ssn);
+        sqlOption.addWhereFormat(ConditionTypeConst.OR, "email = '%s'", email);
+        sqlOption.addWhereFormat(ConditionTypeConst.OR, "(bank_account_no = '%s' AND bank_routing_no = '%s')", accountNo, routingNo);
+        sqlOption.addField(JsonKeyConst.ID);
+        sqlOption.addOrder("create_time DESC");
+        sqlOption.addPage("LIMIT 1");
+        return customerRepository.listCustomerByOptions(sqlOption.toString());
+    }
+
+    private String findCustomerByCondition(String conditionStr) {
+        return customerRepository.findCustomerByOptions(conditionStr);
     }
 }
