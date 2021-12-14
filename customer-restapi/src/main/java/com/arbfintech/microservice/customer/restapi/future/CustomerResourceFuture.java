436a0fa2a18c42e3224b49d63b8c287f30915f49
xiexia <crystal@sztus.com>
2021-12-14 17:35:02 +0800

    CommonReader

--- customer-restapi/src/main/java/com/arbfintech/microservice/customer/restapi/future/CustomerResourceFuture.java
+++ customer-restapi/src/main/java/com/arbfintech/microservice/customer/restapi/future/CustomerResourceFuture.java
@@ -4,9 +4,12 @@ import com.arbfintech.framework.component.core.type.AjaxResult;
 import com.arbfintech.framework.component.core.type.ProcedureException;
 import com.arbfintech.microservice.customer.object.constant.CustomerFeatureKey;
 import com.arbfintech.microservice.customer.object.dto.CustomerProfileDTO;
+import com.arbfintech.microservice.customer.object.entity.CustomerProfile;
 import com.arbfintech.microservice.customer.object.enumerate.CustomerErrorCode;
+import com.arbfintech.microservice.customer.restapi.repository.reader.CommonReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.CompletableFuture;
@@ -20,13 +23,16 @@ public class CustomerResourceFuture {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerResourceFuture.class);
 
+    @Autowired
+    private CommonReader commonReader;
+
     public CompletableFuture<String> getProfileByFeature(CustomerProfileDTO customerProfileDTO) {
         return CompletableFuture.supplyAsync(
                 () -> {
                     try {
                         switch (customerProfileDTO.getProfileFeature()) {
                             case CustomerFeatureKey.PERSONAL: {
-                                return null;
+                                return AjaxResult.success(commonReader.getEntityByCustomerId(CustomerProfile.class, customerProfileDTO.getCustomerId()));
                             }
                             case CustomerFeatureKey.EMPLOYMENT: {
                                 return null;
--- customer-restapi/src/main/java/com/arbfintech/microservice/customer/restapi/repository/reader/CommonReader.java
+++ customer-restapi/src/main/java/com/arbfintech/microservice/customer/restapi/repository/reader/CommonReader.java
@@ -3,6 +3,7 @@ package com.arbfintech.microservice.customer.restapi.repository.reader;
 import com.arbfintech.framework.component.core.constant.ConditionTypeConst;
 import com.arbfintech.framework.component.core.type.SqlOption;
 import com.arbfintech.framework.component.database.core.BaseJdbcReader;
+import com.arbfintech.microservice.customer.object.entity.CustomerOptIn;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +15,15 @@ public class CommonReader extends BaseJdbcReader {
         SqlOption sqlOption = SqlOption.getInstance();
         sqlOption.whereFormat(ConditionTypeConst.AND, "customer_id = '%d'", customerId);
         return findAllByOptions(tClass, sqlOption.toString());
+    }
+
+    public <T> T getEntityByCustomerId(Class<T> tClass, Long customerId) {
+        SqlOption sqlOption = SqlOption.getInstance();
+        if (tClass.equals(CustomerOptIn.class)) {
+            sqlOption.whereFormat(ConditionTypeConst.AND, "customer_id = '%d'", customerId);
+        } else {
+            sqlOption.whereFormat(ConditionTypeConst.AND, "id = '%d'", customerId);
+        }
+        return findByOptions(tClass, sqlOption.toString());
     }
 }
