3144500bc052d0bf825ed18370464e08162da185
xiexia <crystal@sztus.com>
2022-01-21 15:00:25 +0800

    crypto

--- customer-restapi/src/main/java/com/arbfintech/microservice/customer/restapi/repository/reader/CommonReader.java
+++ customer-restapi/src/main/java/com/arbfintech/microservice/customer/restapi/repository/reader/CommonReader.java
@@ -1,6 +1,7 @@
 package com.arbfintech.microservice.customer.restapi.repository.reader;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.arbfintech.framework.component.core.constant.ConditionTypeConst;
 import com.arbfintech.framework.component.core.type.SqlOption;
@@ -9,6 +10,7 @@ import com.arbfintech.microservice.customer.object.entity.CustomerOptIn;
 import com.arbfintech.microservice.customer.object.util.AESCryptoUtil;
 import com.arbfintech.microservice.customer.object.util.CustomerFieldKey;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -18,7 +20,23 @@ public class CommonReader extends BaseJdbcReader {
     public <T> List<T> listEntityByCustomerId(Class<T> tClass, Long customerId) {
         SqlOption sqlOption = SqlOption.getInstance();
         sqlOption.whereFormat(ConditionTypeConst.AND, "customer_id = '%d'", customerId);
-        return findAllByOptions(tClass, sqlOption.toString());
+
+        List<T> entityList = findAllByOptions(tClass, sqlOption.toString());
+        if (CollectionUtils.isEmpty(entityList)) {
+            return null;
+        }
+
+        if (CustomerFieldKey.getEncodeClassList().contains(tClass)) {
+            JSONArray resultArray = new JSONArray();
+            for (T entity : entityList) {
+                JSONObject entityJson = JSON.parseObject(JSON.toJSONString(entity));
+                JSONObject decryptJson = AESCryptoUtil.decryptData(entityJson);
+                resultArray.add(decryptJson);
+            }
+            return resultArray.toJavaList(tClass);
+        } else {
+            return entityList;
+        }
     }
 
     public <T> T getEntityByCustomerId(Class<T> tClass, Long customerId) {
