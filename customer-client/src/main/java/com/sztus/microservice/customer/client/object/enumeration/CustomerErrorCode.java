089be88cf56725e11ed97ac8cff38fafc93c02e6
jiangTestT <1663402304@qq.com>
2022-03-04 18:16:06 +0800

    Error Code

--- customer-client/src/main/java/com/sztus/microservice/customer/client/object/enumeration/CustomerErrorCode.java
+++ customer-client/src/main/java/com/sztus/microservice/customer/client/object/enumeration/CustomerErrorCode.java
@@ -4,8 +4,8 @@ import com.sztus.framework.component.core.base.BaseError;
 
 public enum CustomerErrorCode implements BaseError {
 
-    CUSTOMER_IS_NOT_EXISTED(-95091001, "Expected customer is not existed"),
-    FAILURE_PARAMETER_IS_INCOMPLETE(-95091002, "Parameter(s) is(are) incomplete."),;
+    CUSTOMER_IS_NOT_EXISTED(95091001, "Expected customer is not existed"),
+    FAILURE_PARAMETER_IS_INCOMPLETE(95091002, "Parameter(s) is(are) incomplete."),;
 
     private Integer code;
     private String message;
