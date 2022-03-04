15880bbf4a77ad287be4b96e9f564f82b3c38521
Vincent Cheung <vincent@arbfintech.com>
2022-03-04 22:42:04 +0800

    modify details

--- customer-server/src/main/java/com/sztus/microservice/customer/server/constant/CustomerJsonKey.java
+++ customer-server/src/main/java/com/sztus/microservice/customer/server/constant/CustomerJsonKey.java
@@ -1,9 +1,5 @@
 package com.sztus.microservice.customer.server.constant;
 
-/**
- * @author Fly_Roushan
- * @date 2021/1/4
- */
 public interface CustomerJsonKey {
 
     String ACCOUNT_NO = "accountNo";
