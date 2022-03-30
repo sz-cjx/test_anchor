29d500b0a6abaeb293f33e18f1254ef800cc6f9a
Baron <baron@arbfintech.com>
2022-03-30 17:02:01 +0800

    PAN-4860 Customer Create and Query

--- customer-server/src/main/java/com/sztus/dalaran/microservice/customer/server/converter/CustomerConverter.java
+++ customer-server/src/main/java/com/sztus/dalaran/microservice/customer/server/converter/CustomerConverter.java
@@ -1,9 +1,13 @@
 package com.sztus.dalaran.microservice.customer.server.converter;
 
+import com.sztus.dalaran.microservice.customer.client.object.parameter.request.CreateCustomerRequest;
+import com.sztus.dalaran.microservice.customer.client.object.parameter.response.CreateCustomerResponse;
+import com.sztus.dalaran.microservice.customer.client.object.parameter.response.GetCustomerByUniqueResponse;
 import com.sztus.dalaran.microservice.customer.client.object.parameter.response.GetCustomerResponse;
 import com.sztus.dalaran.microservice.customer.client.object.parameter.response.SaveCustomerResponse;
 import com.sztus.dalaran.microservice.customer.client.object.view.CustomerView;
 import com.sztus.dalaran.microservice.customer.server.domain.Customer;
+import com.sztus.dalaran.microservice.customer.server.domain.CustomerPersonalData;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -13,6 +17,14 @@ public interface CustomerConverter {
     GetCustomerResponse CustomerToGetCustomerResponse(Customer customer);
 
     SaveCustomerResponse CustomerToSaveCustomerResponse(Customer customer);
+
+    CreateCustomerResponse CustomerToCreateCustomerResponse(Customer customer);
+
+    GetCustomerByUniqueResponse CustomerToGetCustomerByUniqueResponse(Customer customer);
+
+    CustomerPersonalData CreateCustomerRequestToCustomerProfile(CreateCustomerRequest request);
+
+    Customer CreateCustomerRequestToCustomer(CreateCustomerRequest request);
 
     Customer CustomerViewToCustomer(CustomerView customerView);
 
