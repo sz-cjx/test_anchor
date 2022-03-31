883540e9180f32ffcc59318a45ca41a91e1c7fa9
luffy <1179095884@qq.com>
2022-03-31 15:47:47 +0800

    Add lead filter

---
+++ customer-client/src/main/java/com/sztus/dalaran/microservice/customer/client/object/view/CustomerPersonalDataView.java
@@ -0,0 +1,159 @@
+package com.sztus.dalaran.microservice.customer.client.object.view;
+
+/**
+ * @author Luffy
+ * @description
+ * @createTime 2022年03月30日 19:15:00
+ */
+public class CustomerPersonalDataView {
+
+    private Long customerId;
+
+    private String ssn;
+
+    private String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    private Integer gender;
+
+    private String birthday;
+
+    private String address;
+
+    private String city;
+
+    private String state;
+
+    private String zip;
+
+    private Integer military;
+
+    private Integer language;
+
+    private Long createdAt;
+
+    private Long updatedAt;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public Integer getMilitary() {
+        return military;
+    }
+
+    public void setMilitary(Integer military) {
+        this.military = military;
+    }
+
+    public Integer getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Integer language) {
+        this.language = language;
+    }
+
+    public Long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Long updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
