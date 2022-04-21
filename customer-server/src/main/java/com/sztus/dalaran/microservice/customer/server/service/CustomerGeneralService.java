0fe605ecd464304a34db8489ecc764b80f6c5b37
Beal Lu <beal@sztus.com>
2022-04-21 14:25:12 +0800

    Phoenix - Complete Personal related functions

--- customer-server/src/main/java/com/sztus/dalaran/microservice/customer/server/service/CustomerGeneralService.java
+++ customer-server/src/main/java/com/sztus/dalaran/microservice/customer/server/service/CustomerGeneralService.java
@@ -5,6 +5,9 @@ import com.sztus.dalaran.microservice.customer.client.object.parameter.enumerate
 import com.sztus.dalaran.microservice.customer.server.domain.Customer;
 import com.sztus.dalaran.microservice.customer.server.domain.CustomerContactData;
 import com.sztus.dalaran.microservice.customer.server.domain.CustomerPersonalData;
+import com.sztus.dalaran.microservice.customer.server.respository.reader.CustomerReader;
+import com.sztus.dalaran.microservice.customer.server.respository.writer.CustomerWriter;
+import com.sztus.dalaran.microservice.customer.server.util.CustomerCheckUtil;
 import com.sztus.framework.component.core.constant.StatusConst;
 import com.sztus.framework.component.core.type.ProcedureException;
 import com.sztus.framework.component.database.constant.ConditionTypeConst;
@@ -19,31 +22,53 @@ import java.util.Objects;
 public class CustomerGeneralService {
 
     @Autowired
-    private SimpleProcedure simpleProcedure;
+    private CustomerReader customerReader;
+
+    @Autowired
+    private CustomerWriter customerWriter;
+
 
     public Customer getCustomerByCustomerId(Long customerId) {
-        return simpleProcedure.findById(Customer.class, customerId);
+        return customerReader.findById(Customer.class, customerId, null);
     }
 
     public CustomerContactData getCustomerContactByContact(String contactInformation) {
         SqlOption sqlOption = SqlOption.getInstance();
         sqlOption.whereFormat(ConditionTypeConst.AND, "contact_information = '%s'", contactInformation);
-        return simpleProcedure.findByOptions(CustomerContactData.class, sqlOption.toString());
+        return customerReader.findByOptions(CustomerContactData.class, sqlOption.toString());
     }
 
+    public void saveCustomer(Customer customer) throws ProcedureException {
+        Long id = customer.getId();
+        String openId = customer.getOpenId();
+
+        Customer customerDb = customerReader.getCustomerByCondition(id, openId);
+        if (Objects.nonNull(customerDb)) {
+            customer.setId(customerDb.getId());
+            customer.setOpenId(customerDb.getOpenId());
+        }
+
+        Long result = customerWriter.save(Customer.class, JSON.toJSONString(customer));
+        CustomerCheckUtil.checkSaveResult(result);
+        if (!Objects.equals(result, 1L)) {
+            customer.setId(result);
+        }
+    }
+
+
     public CustomerPersonalData getCustomerPersonalDataByCustomerId(Long customerId) throws ProcedureException {
-        if (Objects.isNull(customerId)){
+        if (Objects.isNull(customerId)) {
             throw new ProcedureException(CustomerErrorCode.PARAMETER_IS_INCOMPLETE);
         }
         SqlOption instance = SqlOption.getInstance();
-        instance.whereFormat(ConditionTypeConst.AND,"customer_id= %d",customerId);
-        return simpleProcedure.findByOptions(CustomerPersonalData.class,instance.toString());
+        instance.whereFormat(ConditionTypeConst.AND, "customer_id= %d", customerId);
+        return customerReader.findByOptions(CustomerPersonalData.class, instance.toString());
     }
 
     public Long saveCustomerPersonalData(CustomerPersonalData personalData) throws ProcedureException {
-        if (Objects.isNull(personalData.getCustomerId())){
-            throw  new ProcedureException(CustomerErrorCode.PARAMETER_IS_INCOMPLETE);
+        if (Objects.isNull(personalData.getCustomerId())) {
+            throw new ProcedureException(CustomerErrorCode.PARAMETER_IS_INCOMPLETE);
         }
-       return simpleProcedure.save(CustomerPersonalData.class, JSON.toJSONString(personalData));
+        return customerWriter.save(CustomerPersonalData.class, JSON.toJSONString(personalData));
     }
 }
