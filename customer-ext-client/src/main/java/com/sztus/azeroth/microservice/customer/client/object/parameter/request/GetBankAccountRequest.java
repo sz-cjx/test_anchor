a0d69c883f097d8a7919d233bbef405c2036f005
Niklaus <klaus@sztus.com>
2022-06-27 21:29:22 +0800

    updated

--- customer-ext-client/src/main/java/com/sztus/azeroth/microservice/customer/client/object/parameter/request/GetBankAccountRequest.java
+++ customer-ext-client/src/main/java/com/sztus/azeroth/microservice/customer/client/object/parameter/request/GetBankAccountRequest.java
@@ -3,6 +3,16 @@ package com.sztus.azeroth.microservice.customer.client.object.parameter.request;
 public class GetBankAccountRequest {
     private Long id;
 
+    private Long customerId;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
     public Long getId() {
         return id;
     }
